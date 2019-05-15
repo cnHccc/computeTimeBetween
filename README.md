eb095dcbdd2453a14c4b6cf351fdca8fa46cf472
cnHccc <2456450272@qq.com>
2019-05-15 18:09:31 +0800

    Signed-off-by: cnHccc <2456450272@qq.com>

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ##计算两个时间戳之间相差
 ```javascript
 //示例
-residue(1557676800,1557676800);
+residue(1557676800,1557676800,format = '${d}天${h}小时${m}分钟${s}秒',config = {
+    complete:true
+});
 //输出
 '5天0小时0分钟0秒'
+```
+###config配置
+```javascript
+complete = true;//如果单数前面自动补0
 ```
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // start:number|string 时间戳(单位:秒)
 // end:number|string 时间戳(单位:秒)
 // format:string 正则匹配规则 ${d}天 ${h}小时 ${m}分钟 ${s}秒
-function residue(start, end, format = '${d}天${h}小时${m}分钟${s}秒'){
+function residue(start, end, format = '${d}天${h}小时${m}分钟${s}秒',config = {}){
     //开始时间与结束时间 单位秒（不是毫秒）
     start = parseInt(start); //转换成number
     end = parseInt(end); //转换成number
@@ -14,12 +14,21 @@ function residue(start, end, format = '${d}天${h}小时${m}分钟${s}秒'){
     between = between % 86400;
     // 计算小时
     let hour = Math.floor(between / 3600);
+    if (config.complete){
+        hour < 10 ? hour = "0" + hour:hour;
+    }
     between = between % 3600;
     // 计算分钟
     let minute = Math.floor(between / 60);
+    if (config.complete) {
+        minute < 10 ? minute = "0" + minute : minute;
+    }
     between = between % 60;
     //计算秒
     let second = between;
+    if (config.complete) {
+        second < 10 ? second = "0" + second : second;
+    }
     //正则转换天
     format = format.replace(/\${d}/, day);
     //正则转换小时
